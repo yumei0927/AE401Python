28c7e50373114cb98d3525feacc30841072bde60
yumei0927 <78334448+yumei0927@users.noreply.github.com>
2021-02-02 08:44:34 +0800

    Add files via upload

---
+++ score.py
@@ -0,0 +1,12 @@
+score=int(input('請輸入成績'))
+if score>=90:
+          print('A')
+elif score>=80:
+          print('B')
+elif score>=70:
+          print('C')
+elif score>=60:
+          print('D')
+else:
+          print('E')
+          
