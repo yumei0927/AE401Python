433b1226282b7c745856df8ae241671b860f3d00
yumei0927 <78334448+yumei0927@users.noreply.github.com>
2021-02-02 08:44:46 +0800

    Add files via upload

---
+++ 圓形.py
@@ -0,0 +1,14 @@
+radius=input('請輸入圓的半徑')
+radius=float(radius)
+x=float(3.1415926)
+print('圓周率為',x)
+print('面積為',radius*radius*3.1415926)
+print('圓周長為',radius*2*3.1415926)
+
+
+
+radius=float(input('請輸入圓的半徑'))
+x=float(3.1415926)
+print('圓周率為',x)
+print('面積為',radius*radius*x)
+print('圓周長為',radius*2*x)
